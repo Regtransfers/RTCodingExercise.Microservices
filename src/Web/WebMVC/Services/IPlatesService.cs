340f016967f565cfae3ecd7e9114b8312cea16cf
Mike Adam <22458087+mike-adam-personal@users.noreply.github.com>
2024-10-27 17:32:19 +0000

    Add MVC PlatesService to get data from Catalog.API.

---
+++ src/Web/WebMVC/Services/IPlatesService.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.DTO;
+
+namespace WebMVC.Services
+{
+    public interface IPlatesService
+    {
+        Task<PlateListResponse> GetPlatesAsync(int pageNumber = 1);
+
+        Task<CreateUpdateResult> CreatePlateAsync(PlateCreateRequest plate);
+    }
+}
