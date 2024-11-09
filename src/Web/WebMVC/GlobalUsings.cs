2866175005050fec5c19c7118318f4739e773e4a
DBloodworth95 <dan.bloodworth95@gmail.com>
2024-11-09 17:18:23 +0000

    chore: setup rabbitmq

--- src/Web/WebMVC/GlobalUsings.cs
+++ src/Web/WebMVC/GlobalUsings.cs
@@ -12,7 +12,6 @@ global using Microsoft.Extensions.Hosting;
 global using Microsoft.Extensions.Logging;
 global using Microsoft.Extensions.Options;
 global using Polly;
-global using RTCodingExercise.WebMVC;
 global using Catalog.Domain;
 global using Serilog;
 global using System;
--- src/Web/WebMVC/Program.cs
+++ src/Web/WebMVC/Program.cs
@@ -1,3 +1,5 @@
+using WebMVC;
+
 string Namespace = typeof(Startup).Namespace;
 string AppName = Namespace.Substring(Namespace.LastIndexOf('.', Namespace.LastIndexOf('.') - 1) + 1);
 
